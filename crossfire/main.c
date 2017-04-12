9eaa39fb9e340cdddf946f47bbb000fd61c2ff61
charleskelly13 <charliekelly1997@hotmail.com>
2017-04-12 18:58:51 +0100

    fixing attack

--- crossfire/main.c
+++ crossfire/main.c
@@ -50,7 +50,67 @@ int main()
 	//			->which attack
 	//	->move
 	//	->quit
-	for(i=0; i<Player_Num && death>1 ; i++) //move or attack
+
+
+
+
+	/*If the the required slot is closer to the down-right
+	 * corner of the board the search starts from downRight,
+	 * which points to slot at position (boardSize-1, boarSize -1)*/
+	if(row >= BOARDSIZE/2)
+	{
+		if(column >= BOARDSIZE/2)													//forget about downright etc
+		{																			//starts in player position
+			currSlot = reachDesiredElement(row,column,downRight);					//
+		}
+		else
+		{
+			/*If the the required slot is closer to the down-left
+			 * corner of the board the search starts from downLeft,
+			 * which points to slot at position (boardSize-1, 0)*/
+			currSlot = reachDesiredElement(row,column,downLeft);
+		}
+	}
+	else
+	{
+		/*If the the required slot is closer to the up-right
+		* corner of the board the search starts from upRight,
+		* which points to slot at position (0, boarSize -1)*/
+		if(column >= BOARDSIZE/2)
+		{
+			currSlot = reachDesiredElement(row,column, upRight);
+			/*If the the required slot is closer to the up-left
+			* corner of the board the search starts from upLeft,
+			* which points to slot at position (0, 0)*/
+		}
+		else
+		{
+			currSlot = reachDesiredElement(row,column,upLeft);
+		}
+	}
+
+	for(int i=0; i<BOARDSIZE; i++)
+	{
+		for(int j=0; j<BOARDSIZE;j++)
+		{
+			explored[i][j] = false;
+		}
+	}
+
+	foundSlots = malloc(BOARDSIZE * BOARDSIZE * sizeof(struct slot ));
+	printf("\n\nFunction findSlotsinvoked:\n");
+
+	if(currSlot!= NULL)
+	{
+		//invokes function findSlots. The required distance is a constant
+		//However you can reuse this function providing as input any arbitrary distance
+		findSlots(REQ_DISTANCE, 0, currSlot, foundSlots, &count, explored);
+		for(int i=0; i<count; i++)
+		{
+			printf("(%d, %d)-> ",foundSlots[i].row, foundSlots[i].column);
+		}
+	}
+		for(i=0; i<Player_Num && death>1 ; i++) //move or attack
 		{
 			if(status[i]!=dead && status[i]!=quit)
 			{
@@ -124,8 +184,10 @@ int main()
 						else if(Player[k].PlaceColumn==Player[i].PlaceColumn&&(Player[k].PlaceRow==Player[i].PlaceRow||Player[k].PlaceRow==Player[i].PlaceRow+1||Player[k].PlaceRow==Player[i].PlaceRow-1))
 						{
 							abl[k]=1;
+							
 							u=u+1;
 						}
+						k=k+1;
 						}
 					}
 					k=0;
@@ -143,6 +205,7 @@ int main()
 						{
 							printf("%d", k+1);
 						}
+						k=k+1;
 					}
 					printf("Choose which player to attack");
 					scanf("%d", &e);
@@ -204,91 +267,10 @@ int main()
 				}
 			}
 			printf("\n");
-
-
-			/*			for(j=0; j<slot_no; j++)
-			{
-				for(k=0; k<PlayerNum; k++)
-				{
-					if(slot[j].place==Player[k].Place && status[k]!=quit && status[k]!=dead)
-					{
-						printf("P%d   ", k+1);
-					}
-					else
-					{
-						counter++;
-					}
-					if(counter==PlayerNum)
-					{
-						printf("_   ");
-					}
-				}
-				counter=0;
-			}*/
+			
 		}
 	}
-
-
-
-
-
-
-	/*If the the required slot is closer to the down-right
-	 * corner of the board the search starts from downRight,
-	 * which points to slot at position (boardSize-1, boarSize -1)*/
-	if(row >= BOARDSIZE/2)
-	{
-		if(column >= BOARDSIZE/2)													//forget about downright etc
-		{																			//starts in player position
-			currSlot = reachDesiredElement(row,column,downRight);					//
-		}
-		else
-		{
-			/*If the the required slot is closer to the down-left
-			 * corner of the board the search starts from downLeft,
-			 * which points to slot at position (boardSize-1, 0)*/
-			currSlot = reachDesiredElement(row,column,downLeft);
-		}
-	}
-	else
-	{
-		/*If the the required slot is closer to the up-right
-		* corner of the board the search starts from upRight,
-		* which points to slot at position (0, boarSize -1)*/
-		if(column >= BOARDSIZE/2)
-		{
-			currSlot = reachDesiredElement(row,column, upRight);
-			/*If the the required slot is closer to the up-left
-			* corner of the board the search starts from upLeft,
-			* which points to slot at position (0, 0)*/
-		}
-		else
-		{
-			currSlot = reachDesiredElement(row,column,upLeft);
-		}
-	}
-
-	for(int i=0; i<BOARDSIZE; i++)
-	{
-		for(int j=0; j<BOARDSIZE;j++)
-		{
-			explored[i][j] = false;
-		}
-	}
-
-	foundSlots = malloc(BOARDSIZE * BOARDSIZE * sizeof(struct slot ));
-	printf("\n\nFunction findSlotsinvoked:\n");
-
-	if(currSlot!= NULL)
-	{
-		//invokes function findSlots. The required distance is a constant
-		//However you can reuse this function providing as input any arbitrary distance
-		findSlots(REQ_DISTANCE, 0, currSlot, foundSlots, &count, explored);
-		for(int i=0; i<count; i++)
-		{
-			printf("(%d, %d)-> ",foundSlots[i].row, foundSlots[i].column);
-		}
-	}
+	
 
 	return 0;
 }
